7aa5f9da972603ac3cf4253f11ecd0fa143f5582
Tim Messaoudene <messaoudene-tim@outlook.fr>
2022-02-02 12:21:33 +0100

    Connexion page

--- client/src/pages/Connexion.jsx
+++ client/src/pages/Connexion.jsx
@@ -1,21 +1,44 @@
 import React from "react";
+import useForm from "../hooks/useForm";
+import connexionValidate from "../utils/formValidate/connexionValidate";
 
+const INITIAL_STATE = {
+    "email": '',
+    "password": ''
+}
 function Connexion ()
 {
+    const {handleChange, handleSubmit, errors, isSubmitted} = useForm(INITIAL_STATE, connexionValidate());
+
+    const onSubmit = (value) =>
+    {
+        console.log(value);
+    }
+
     return <main>
         <h1>Connexion</h1>
-        <form>
+        <form onSubmit={(e) => handleSubmit(onSubmit, e)}>
             <div className="box-input">
                 <label htmlFor="email">
                     Email :
-                    <input type="text" name="email" placeholder="Votre email" required="required"/>
+                    <input type="text" name="email" placeholder="Votre email" required="required" onChange={handleChange}/>
                 </label>
+                <div className="box-errors">
+                    {
+                        isSubmitted && errors.emailEmpty
+                    }
+                </div>
             </div>
             <div className="box-input">
                 <label htmlFor="password">
                     Password :
-                    <input type="text" name="password" placeholder="Votre mot de passe" required="required"/>
+                    <input type="text" name="password" placeholder="Votre mot de passe" required="required" onChange={handleChange}/>
                 </label>
+            </div>
+            <div className="box-errors">
+                {
+                    isSubmitted && errors.passwordEmpty
+                }
             </div>
             <input type="submit" value="Envoyer" />
         </form>
