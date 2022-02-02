89eebae6a5666f3970997c79d07d07b1bc833e33
StevyDevWeb <stevywebdev@gmail.com>
2022-02-02 14:56:10 +0100

    server

---
+++ server/schemas/instrumentSchema.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const InstrumentSchema = mongoose.Schema({
+    name: {type: String, required: true},
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    },
+    type: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'typeInstrument',
+        required: true
+    },
+    image: {type: String, required: true},
+    description: {type: String, required: true},
+    borrow: {type: Boolean, required: true, default: false}
+})
+
+module.exports = mongoose.model("instrument", InstrumentSchema);
