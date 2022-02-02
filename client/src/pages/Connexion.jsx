ee193b81800a346e4cbf31121da0833ec94aefa8
Tim MESSAOUDENE <messaoudene-tim@outlook.fr>
2022-02-02 11:37:02 +0100

    Component Connexion

--- client/src/pages/Connexion.jsx
+++ client/src/pages/Connexion.jsx
@@ -2,7 +2,25 @@ import React from "react";
 
 function Connexion ()
 {
-    return <main></main>
+    return <main>
+        <h1>Connexion</h1>
+        <form>
+            <div className="box-input">
+                <label htmlFor="email">
+                    Email :
+                    <input type="text" name="email" placeholder="Votre email" required="required"/>
+                </label>
+            </div>
+            <div className="box-input">
+                <label htmlFor="password">
+                    Password :
+                    <input type="text" name="password" placeholder="Votre mot de passe" required="required"/>
+                </label>
+            </div>
+            <input type="submit" value="Envoyer" />
+        </form>
+        <a href="#">Mot de passe oublié ?</a>
+    </main>
 }
 
 export default Connexion;
